9267a71d3486eec4af5fd489038e1d8c5ce5d3f3
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-23 22:23:03 -0700

    Commiting changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import TopScreen from ".././screens/TopScreen";
 
 class App extends Component {
   render() {
     return (
       <div>
         <div>App</div>
+        <TopScreen />
       </div>
     );
   }
