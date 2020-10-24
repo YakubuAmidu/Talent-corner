dbc14c4f2d79140681f3458fe9b3c93b58009d83
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-23 22:26:56 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createBrowserHistory } from "history";
+import { Switch, Route, Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router history={createBrowserHistory}>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
