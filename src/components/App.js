d542e56a00639c359f897cc39b61608f56e316d8
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-24 18:31:18 -0700

    Commiting changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 import TopScreen from "../screens/TopScreen";
-import ImageScreen from ".././screens/ImageScreen";
+import background from ".././assets/background.png";
 import "./Sass.scss";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <TopScreen />
-          <ImageScreen />
+      <div className="App_header">
+        <TopScreen />
+
+        <div className="Background_image">
+          <img
+            className="Background_image-main"
+            src={background}
+            alt="background-img"
+          />
+          <div className="Background_title">
+            <div>FRESH</div>
+            <div>TALENT +</div>
+            <div>TOP</div>
+            <div>EMPLOYERS</div>
+          </div>
+          <div>create your future with us</div>
+          <span className="Link_buttons">
+            <Row>
+              <Col>
+                <Button>FIND TALENT</Button>
+              </Col>
+              <Col>
+                <Button>FIND TALENT</Button>
+              </Col>
+            </Row>
+          </span>
         </div>
       </div>
     );
