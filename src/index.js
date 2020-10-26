7e1b27af95bdda05ebdfbc79b0aa9c23d64cdea4
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-25 23:08:25 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Gold from "./components/Gold";
 import TalentScreen from "./screens/TalentScreen";
 import { createBrowserHistory } from "history";
 import { Router, Switch, Route } from "react-router-dom";
--- src/screens/TalentScreen.js
+++ src/screens/TalentScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Links from ".././components/Links";
+import VideoOne from "../assets/VideoOne.mp4";
 
 const TalentScreen = () => {
   return (
@@ -9,6 +10,7 @@ const TalentScreen = () => {
         <h1 className="Find">+ FIND TALENT</h1>
         <p className="Dream">Your dream candidate is just around the corner</p>
       </div>
+      <video src={VideoOne} autoPlay="true" className="Background_video" />
     </div>
   );
 };
