c360fd5d3f3e5250ca187fcb1dcfe5ab3c98e2f4
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-10-26 01:15:09 -0700

    Commiting changes

--- src/screens/TalentScreen.js
+++ src/screens/TalentScreen.js
@@ -17,7 +17,7 @@ const TalentScreen = () => {
         <img src={Profile} alt="profile-img" className="Profile_image_one" />
 
         <Row>
-          <Col>
+          <Col className="Talent_header_one">
             <p className="Talent_paragraph_one">
               I'm a paragraph. Click here to add your own text and edit me. It’s
               easy. Just click “Edit Text” or double click me to add your own
@@ -25,7 +25,7 @@ const TalentScreen = () => {
               me anywhere you like on your page. I’m a great place for you to
               tell a story and let your users know a little more about you.
             </p>
-            <p className="Talent_paragraph_one">
+            <p className="Talent_paragraph_two">
               I'm a paragraph. Click here to add your own text and edit me. It’s
               easy. Just click “Edit Text” or double click me to add your own
               content and make changes to the font. Feel free to drag and drop
@@ -33,13 +33,13 @@ const TalentScreen = () => {
               tell a story and let your users know a little more about you.
             </p>
           </Col>
-          <Col>
-            <p className="Talent_paragraph_two">
+          <Col className="Talent_header_two">
+            <p className="Talent_paragraph_three">
               We can't wait to hear from you! Call us today and let us help you
               find what you're looking for.
             </p>
             <hr />
-            <Button className="Btn btn-warning">CONTACT</Button>
+            <Button className="btn btn-warning Btn_warning">CONTACT</Button>
           </Col>
         </Row>
       </div>
